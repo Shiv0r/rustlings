a36e1a6973056523d66cd55e62769565ef6dba43
Shiv0r <edward.agwi@outlook.de>
2026-02-20 09:33:03 +0100

    struct section done

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -1,5 +1,4 @@
 struct ColorRegularStruct {
-    // TODO: Add the fields that the test `regular_structs` expects.
     // What types should the fields have? What are the minimum and maximum values for RGB colors?
     red: i32,
     green: i32,
@@ -18,7 +17,6 @@ mod tests {
 
     #[test]
     fn regular_structs() {
-        // TODO: Instantiate a regular struct.
         // let green =
         let green = ColorRegularStruct {
             red: 0,
@@ -33,7 +31,6 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct.
         // let green =
 
         let green: ColorTupleStruct = ColorTupleStruct(0, 255, 0);
@@ -45,7 +42,6 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit struct.
         // let unit_struct =
         let unit_struct = UnitStruct;
         let message = format!("{unit_struct:?}s are fun!");
